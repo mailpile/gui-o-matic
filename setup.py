715581aa6c1b0b3f948aedf8d18d31127ffd6de2
Bjarni R. Einarsson <bre@klaki.net>
2016-03-25 00:45:31 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+
+setup(
+    name='gui_o_matic',
+    version='0.1',
+    author='',
+    author_email='',
+    packages=['gui_o_matic'],
+    url='',
+    license='See LICENSE.txt',
+    description='',
+    long_description=open('README.md').read(),
+)
