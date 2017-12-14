418b8e17408ec160d2f1942f88751dde00d8706c
Bjarni R. Einarsson <bre@klaki.net>
2017-12-14 16:33:24 +0100

    1st draft of a TCP-socket based control channel

--- gui_o_matic/control/__init__.py
+++ gui_o_matic/control/__init__.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import socket
 import time
 import threading
 import traceback
@@ -11,6 +12,8 @@ class GUIPipeControl(threading.Thread):
     OK_GO = 'OK GO'
     OK_LISTEN = 'OK LISTEN'
     OK_LISTEN_TO = 'OK LISTEN TO:'
+    OK_LISTEN_TCP = 'OK LISTEN TCP:'
+    PIVOT_TCP = 'PIVOT TCP:'
     PIVOT_TO = 'PIVOT TO:'
 
     def __init__(self, fd, config=None, gui_object=None):
@@ -20,6 +23,7 @@ class GUIPipeControl(threading.Thread):
         self.gui = gui_object
         self.fd = fd
         self.child = None
+        self.listening = None
 
     def shell_pivot(self, command):
         self.child = subprocess.Popen(command,
@@ -29,6 +33,14 @@ class GUIPipeControl(threading.Thread):
             stdout=subprocess.PIPE)
         self.fd = self.child.stdout
 
+    def shell_tcp_pivot(self, command):
+        self.listening = socket.socket()
+        self.listening.bind(('127.0.0.1', 0))
+        self.listening.listen(0)
+        port = str(self.listening.getsockname()[1])
+        self.shell_pivot(command.replace('%PORT%', port))
+        self.fd = self.listening.accept()[0].makefile()
+
     def bootstrap(self):
         assert(self.config is None)
         assert(self.gui is None)
@@ -37,6 +49,7 @@ class GUIPipeControl(threading.Thread):
         config = []
         while True:
             line = self.fd.readline()
+
             if not line or line.strip() in (self.OK_GO, self.OK_LISTEN):
                 listen = self.OK_LISTEN in line
                 break
@@ -45,6 +58,14 @@ class GUIPipeControl(threading.Thread):
                 self.shell_pivot(line[len(self.OK_LISTEN_TO):].strip())
                 listen = True
                 break
+
+            elif line.startswith(self.OK_LISTEN_TCP):
+                self.shell_tcp_pivot(line[len(self.OK_LISTEN_TCP):].strip())
+                listen = True
+                break
+
+            elif line.startswith(self.PIVOT_TCP):
+                self.shell_tcp_pivot(line[len(self.PIVOT_TCP):].strip())
 
             elif line.startswith(self.PIVOT_TO):
                 self.shell_pivot(line[len(self.PIVOT_TO):].strip())
@@ -70,15 +91,23 @@ class GUIPipeControl(threading.Thread):
                 time.sleep(0.1)
             time.sleep(0.1)
             while True:
-                line = self.fd.readline()
-                if not line:
-                    break
                 try:
-                    cmd, args = line.strip().split(' ', 1)
-                    args = json.loads(args)
-                    self.do(cmd, args)
-                except (ValueError, IndexError, NameError):
-                    traceback.print_exc()
+                    line = self.fd.readline()
+                except IOError:
+                    line = None
+
+                if not line:
+                    if self.listening:
+                        self.fd = self.listening.accept()[0].makefile()
+                    else:
+                        break
+                if line:
+                    try:
+                        cmd, args = line.strip().split(' ', 1)
+                        args = json.loads(args)
+                        self.do(cmd, args)
+                    except (ValueError, IndexError, NameError):
+                        traceback.print_exc()
         except KeyboardInterrupt:
             return
         except:
